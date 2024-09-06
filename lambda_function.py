c803d072176bc34872d81eaf79f91eb545d4f1fd
GRVial <guivial2010@gmail.com>
2024-09-06 15:16:50 -0300

    listar e enviar json

--- lambda_function.py
+++ lambda_function.py
@@ -7,27 +7,42 @@ from refactor_form import get_new_form
 GOOGLE_API_AUTH_FILE = os.environ.get('GOOGLE_API_AUTH_FILE')
 
 def handler(event, context):
+    query_params = event.get('queryStringParameters', {})
+    response_type = query_params.get('type', 'json')
     try:
         ssm = boto3.client('ssm')
         google_credentials_str = ssm.get_parameter(Name=GOOGLE_API_AUTH_FILE)['Parameter']['Value']
         google_credentials = json.loads(google_credentials_str)
         google_api_client = GoogleAPIClient(google_credentials)
-        forms = google_api_client.list_google_forms()
 
-        new_forms_dicts = []
-        for form in forms:
-            form_dict = google_api_client.get_form_json(form['id'])
+        if response_type == 'listForms':
+            forms = google_api_client.list_google_forms()
+            response = {
+                'statusCode': 200,
+                'body': json.dumps(forms, ensure_ascii=False),
+                'headers': {
+                    'Content-Type': 'application/json',
+                },
+            }
+            return response
+        elif response_type == 'json':
+            form_id = query_params.get('formId')
+            form_dict = google_api_client.get_form_json(form_id)
             new_form_dict = get_new_form(form_dict)
-            new_forms_dicts.append(new_form_dict)
-
-        response = {
-            'statusCode': 200,
-            'body': json.dumps(new_forms_dicts, ensure_ascii=False),
-            'headers': {
-                'Content-Type': 'application/json',
-            },
-        }
-        return response
+            response = {
+                'statusCode': 200,
+                'body': json.dumps(new_form_dict, ensure_ascii=False),
+                'headers': {
+                    'Content-Type': 'application/json',
+                },
+            }
+            return response
+        else:
+            response = {
+                'statusCode': 400,
+                'body': 'Tipo de request inválido',
+            }
+            return response
     except Exception as e:
         print('Exception:', e)
         response = {
